1e6abdf973700c34f90d7ea2887e62d16bb98118
paclem <pascal.moeller@posteo.de>
2024-04-03 12:20:56 +0200

    add headline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { HeadlineComponent } from './headline/headline.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, HeadlineComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
