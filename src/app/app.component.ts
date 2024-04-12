62dc81b3f75fbd9273fc6e25e5ebdb7054ffbe1f
paclem <pascal.moeller@posteo.de>
2024-04-12 22:28:45 +0200

    added arrow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { HeadlineComponent } from './headline/headline.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { ArrowComponent } from './arrow/arrow.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HeadlineComponent, AboutMeComponent],
+  imports: [RouterOutlet, HeaderComponent, HeadlineComponent, AboutMeComponent, ArrowComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/arrow/arrow.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-arrow',
+  standalone: true,
+  imports: [],
+  templateUrl: './arrow.component.html',
+  styleUrl: './arrow.component.scss'
+})
+export class ArrowComponent {
+  @Input() direction: 'left' | 'right' = 'left';
+}
