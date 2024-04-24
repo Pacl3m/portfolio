181e61774b5af90c8ae9a017d4e4a208e5290fb2
paclem <pascal.moeller@posteo.de>
2024-04-24 22:48:27 +0200

    added overlay

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,12 @@
 import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  imports: [CommonModule],
 })
 export class HeaderComponent {
 
@@ -22,10 +24,18 @@ export class HeaderComponent {
   onClickOutside(event: MouseEvent) {
     let dropdownMenu = document.querySelector('.dropdownMenu');
     let headerWrapper = document.querySelector('.headerWrapper');
-    let quickLinks = document.querySelector('.quickLinks');
+    let quickLinks = document.querySelectorAll('.quickLinksA');
     let isClickInsideDropdown = dropdownMenu?.contains(event.target as Node);
     let isClickInsideHeaderWrapper = headerWrapper?.contains(event.target as Node);
-    let isClickInsideQuickLinks = quickLinks?.contains(event.target as Node);
+    let isClickInsideQuickLinks = false;
+
+    quickLinks.forEach(element => {
+      if (element.contains(event.target as Node)) {
+        isClickInsideQuickLinks = true;
+        return;
+      }
+    });
+
     if ((!isClickInsideDropdown && !isClickInsideHeaderWrapper) || isClickInsideQuickLinks) {
       this.dropdownMenuActive = false;
       this.toggleAnimation = false;
