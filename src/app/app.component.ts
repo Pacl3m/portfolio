f1b4b6ef020f87f0af1759f8700f3e8dd8b0d7c5
paclem <pascal.moeller@posteo.de>
2024-05-02 21:43:28 +0200

    added my skills component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,17 @@ import { HeaderComponent } from './header/header.component';
 import { HeadlineComponent } from './headline/headline.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { ArrowComponent } from './arrow/arrow.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HeadlineComponent, AboutMeComponent, ArrowComponent],
+  imports: [RouterOutlet,
+    HeaderComponent,
+    HeadlineComponent,
+    AboutMeComponent,
+    ArrowComponent,
+    MySkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
