33f0d8f2d8079d2d550ed39f6f180892b7e12e76
paclem <pascal.moeller@posteo.de>
2024-05-18 19:04:48 +0200

    added ngFor loop in portfolio section

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent {
-
+  projects = [
+    {
+      imgSrc: '/assets/img/portfolio_img/sharkie_portolio.jpg',
+      name: 'Sharkie',
+      skills: 'HTML | CSS | JS',
+      description: 'Ein einfaches Jump-and-Run-Spiel, das auf einem objektorientierten Ansatz basiert. Hilf Sharkie, Münzen und Giftflaschen zu finden, um gegen den Killerwal zu kämpfen.',
+      links: {
+        live: 'Live',
+        github: 'GitHub'
+      },
+      mirrored: false,
+    },
+    {
+      imgSrc: '/assets/img/portfolio_img/myJoinPortfolio.jpg',
+      name: 'Join',
+      skills: 'HTML | CSS | JS | API',
+      description: 'Aufgabenmanager, inspiriert vom Kanban-System. Erstellen und organisieren Sie Aufgaben mithilfe von Drag-and-Drop-Funktionen, weisen Sie Benutzer und Kategorien zu.',
+      links: {
+        live: 'Live',
+        github: 'GitHub'
+      },
+      mirrored: true,
+    },
+    {
+      imgSrc: '/assets/img/portfolio_img/myDABubblePortfolio.jpg',
+      name: 'DABubble',
+      skills: 'Angular | TypeScript | Firebase',
+      description: 'AChat-Messenger im Stil von Slack, Nutzer können sich per Mail oder Google registrieren. In themenbasierten Channels ist der Austausch durch Nachrichten, Bilder oder Reaktionen möglich.',
+      links: {
+        live: 'Live',
+        github: 'GitHub'
+      },
+      mirrored: false,
+    },
+  ];
 }
