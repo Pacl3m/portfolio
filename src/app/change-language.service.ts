9f47753026bb364cf0754735c27b2b127b6af460
paclem <pascal.moeller@posteo.de>
2024-07-28 00:46:12 +0200

    added german/englisch

--- src/app/change-language.service.ts
+++ src/app/change-language.service.ts
@@ -1,12 +1,10 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { __values } from 'tslib';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChangeLanguageService {
-    isEnglish: boolean = true;
+  isEnglish: boolean = true;
 
   constructor() { }
 
